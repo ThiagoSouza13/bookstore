2649f310caac6058d327d33523d9d784db7c0aa1
Thiago Souza <thiago.souza@azos.com.br>
2024-09-30 19:41:01 -0300

    fix

--- product/tests/test_viewsets/test_product_viewset.py
+++ product/tests/test_viewsets/test_product_viewset.py
@@ -2,7 +2,6 @@ import json
 
 from rest_framework import status 
 from rest_framework.test import APITestCase, APIClient 
-from rest_framework.authtoken.models import Token 
 
 from django.urls import reverse 
 
@@ -16,14 +15,11 @@ class TestProductViewSet(APITestCase):
 
     def setUp(self):
         self.user = UserFactory()
-        token = Token.objects.create(user=self.user)
-        token.save()
 
         self.product = ProductFactory(title="pro controller", price=200.00)
 
     def test_get_all_products(self):
-        token = Token.objects.get(user__username=self.user.username)
-        self.client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
+
 
         response = self.client.get(reverse("product-list", kwargs={"version": "v1"}))
 
@@ -35,8 +31,6 @@ class TestProductViewSet(APITestCase):
         self.assertEqual(product_data['results'][0]["active"], self.product.active)
 
     def test_create_product(self):
-        token = Token.objects.get(user__username=self.user.username)
-        self.client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
         category = CategoryFactory()
         data = json.dumps(
             {"title": "notebook", "price": 800, "categories_id": [category.id]}
